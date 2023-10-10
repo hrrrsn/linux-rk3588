a2ea8d0f509049138f6b2c9f10a0c6256eb4bdf1
Harrison Gulliver <harrison.gulliver@spark.co.nz>
2023-10-10 23:34:39 +1300

    Update debian build
    Signed-off-by: Harrison Gulliver <harrison.gulliver@spark.co.nz>

--- scripts/build_debian.sh
+++ scripts/build_debian.sh
@@ -4,10 +4,10 @@
   root_dir=$work_dir/root
 
   debian_version=bookworm
-  # install_desktop="gnome"
+  install_desktop="gnome"
 
   disk_image=$work_dir/$debian_version.img
-  disk_size="5G"
+  disk_size="20G"
 
   echo "Building Debian $debian_version Image..."
 
@@ -32,6 +32,12 @@
   parted $disk_image -- mkpart EFI fat32 1MiB 501MiB
   parted $disk_image -- set 1 boot on
   parted $disk_image -- mkpart primary ext4 501MiB 100%
+
+  efi_uuid=$(uuidgen)
+  root_uuid=$(uuidgen)
+
+  sgdisk -u 1:$efi_uuid $disk_image
+  sgdisk -u 2:$root_uuid $disk_image
   
   # Associate the disk image with a loop device
   losetup -P /dev/loop0 $disk_image
@@ -47,7 +53,7 @@
   mount /dev/loop0p1 $root_dir/boot/efi
   
   # Bootstrap the Debian system
-  debootstrap --arch=arm64 $debian_version $root_dir
+  debootstrap --arch=arm64 $debian_version $root_dir https://mirror.fsmg.org.nz/debian/
 
   # Copy kernels to chroot
   rsync -avh --progress $work_dir/install/ $root_dir/install/
@@ -59,27 +65,38 @@
 
   # Chroot and install kernel, grub and systemd
   chroot $root_dir /bin/bash -c "apt-get update; dpkg -i /install/*.deb; \
-    apt-get install -y grub-efi-arm64 systemd gpg openssh-client nano curl wget initramfs-tools"
+    apt-get install -y grub-efi-arm64 systemd gpgv2"
 
   # Install grub
   mkdir -p $root_dir/boot/efi/EFI/BOOT
-  chroot $root_dir /bin/bash -c "grub-install --target=arm64-efi --efi-directory=/boot/efi --bootloader-id=debian --recheck; update-initramfs -u -k all"
+  chroot $root_dir /bin/bash -c "grub-install --target=arm64-efi --efi-directory=/boot/efi --bootloader-id=debian --recheck"
+
   patch $root_dir/etc/grub.d/10_linux < $work_dir/../../patch/10_linux.patch 
-  chroot $root_dir /bin/bash -c "update-grub; cp /boot/efi/grub/grubaa64.efi /boot/efi/EFI/BOOT/BOOTAA64.EFI"
+  chroot $root_dir /bin/bash -c "update-grub; cp /boot/efi/EFI/debian/grubaa64.efi /boot/efi/EFI/BOOT/BOOTAA64.EFI"
   echo "\EFI\debian\grubaa64.efi" > $root_dir/boot/efi/startup.nsh
 
-  uuid=$(blkid -s UUID -o value /dev/loop0p2)
-  sed -i "s|root=/dev/loop0p2|root=UUID=$uuid|g" $work_dir/root/boot/grub/grub.cfg
-  echo "UUID=$uuid /               ext4    errors=remount-ro 0       1" >> $root_dir/etc/fstab
+  loop_uuid=$(blkid -s UUID -o value /dev/loop0p2)
+  sed -i 's/$loop_uuid/$root_uuid/g' $root_dir/boot/grub/grub.cfg
+  sed -i "s|root=/dev/loop0p2|root=ROOTUUID=$root_uuid|g" $root_dir/boot/grub/grub.cfg
+  echo "UUID=$root_uuid /               ext4    errors=remount-ro 0       1" > $root_dir/etc/fstab
 
   # Install desktop environment
   if [ ! -z "$install_desktop" ]; then
-    chroot $root_dir /bin/bash -c "apt-get install -y $install_desktop"
+    chroot $root_dir /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get install -y $install_desktop"
   fi
+
+  # Set root password
+  chroot $root_dir /bin/bash -c "echo root:password | chpasswd"
+
+  # Set hostname
+  echo "debian-rk3588" > $root_dir/etc/hostname
+
+  # Set timezone
+  chroot $root_dir /bin/bash -c "ln -sf /usr/share/zoneinfo/Pacific/Auckland /etc/localtime"
   
   # Unmount and detach
   rm -rf $root_dir/install
-  chroot $root_dir /bin/bash -c "apt-get clean -y; sync; fstrim -a"
+  chroot $root_dir /bin/bash -c "apt-get clean -y; sync;"
   
   umount $root_dir/{boot/efi,dev,proc,sys}
   umount $root_dir
